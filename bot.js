47e50623779dfd55791d42be6226a13a92a7723d
Devon <devon6608@gmail.com>
2026-03-21 21:21:54 -0700

    Update bot.js
    Removed web server connection code for old use with replit

--- bot.js
+++ bot.js
@@ -1,15 +1,4 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Vargas Bot is running!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Web server is running on port ${PORT}`);
-});
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
