dfb8d04843f0c68810a4a942963642ce160219d8
Devon <devon6608@gmail.com>
2026-03-21 21:30:36 -0700

    Update bot.js
    changed intents section for update to discord.js version 14

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags..MessageContent,
   ]
 });
 
