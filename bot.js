43b5aea64b8a803517055faf621bfd9f13d2ccea
Devon <devon6608@gmail.com>
2026-03-21 21:32:11 -0700

    Update bot.js
    removed extra dot in 3rd intent

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags..MessageContent,
+    IntentsBitField.Flags.MessageContent,
   ]
 });
 
